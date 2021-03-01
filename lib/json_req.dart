19f2fb199a4ee10fc074eaad06abe806cba4f0f7
teagueStockwell <tsAppDevelopment@gmail.com>
2021-02-28 20:05:02 -0800

    dont encode entire obj into api query string

--- lib/json_req.dart
+++ lib/json_req.dart
@@ -4,22 +4,11 @@ import 'package:http/http.dart';
 
 final baseurl = 'http://localhost:8080/fl-api/';
 
-/// calls the get many of an endpoint,
-/// the reqParams are passed as json tp be encoded to paramaters within the Uri
 Future<List<dynamic>> getN(String ep, {Map<String, String> reqParam}) async {
-  String reqParamString = '?';
   Response res;
 
   if (reqParam != null) {
-    for (int i = 0; i < reqParam.length; i++) {
-      reqParamString +=
-          '${reqParam.keys.elementAt(i)}=${reqParam.values.elementAt(i)}';
-      if (i != reqParam.length - 1) {
-        reqParamString += '&';
-      }
-    }
-
-    res = await get(baseurl + ep + reqParamString);
+    res = await get(baseurl + ep + getQueryString(reqParam));
     if (res.statusCode == 200) {
       return jsonDecode(res.body);
     } else {
@@ -36,19 +25,25 @@ Future<List<dynamic>> getN(String ep, {Map<String, String> reqParam}) async {
 }
 
 Future<Response> delete1(String ep, Map<String, dynamic> obj) async {
-  String reqParamString = '?';
-
-  for (int i = 0; i < obj.length; i++) {
-    reqParamString += '${obj.keys.elementAt(i)}=${obj.values.elementAt(i)}';
-    if (i != obj.length - 1) {
-      reqParamString += '&';
-    }
-  }
-
-  return await delete(baseurl + ep + reqParamString);
+  return await delete(baseurl + ep + getQueryString(obj));
 }
 
 List<dynamic> parseJsonIsolate(String json) {
   final ret = jsonDecode(json) as List<dynamic>;
   return ret;
+}
+
+String getQueryString(Map<String,String> reqParam) {
+  String queryString ='?';
+
+  reqParam.removeWhere((k,v) => !k.contains('id'));
+
+   for (int i = 0; i < reqParam.length; i++) {
+      queryString +=
+          '${reqParam.keys.elementAt(i)}=${reqParam.values.elementAt(i)}';
+      if (i != reqParam.length - 1) {
+        queryString += '&';
+      }
+    }
+    return queryString;
 }
